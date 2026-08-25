61e42baf81e37bd6d1f044e6500c842600b92be0
Harshit Jain <jnharshit1998@gmail.com>
2026-08-25 12:21:47 +0000

    fix barcode scanner file linter

--- slbasics2/public/js/barcode_scanner_override.js
+++ slbasics2/public/js/barcode_scanner_override.js
@@ -1,11 +1,14 @@
+/* global erpnext, cur_frm */
+
 frappe.provide("erpnext.utils");
 
-if (erpnext.utils.BarcodeScanner && !erpnext.utils.BarcodeScanner.__cleanup_override) {
-    
+if (
+    erpnext.utils.BarcodeScanner &&
+    !erpnext.utils.BarcodeScanner.__cleanup_override
+) {
     erpnext.utils.BarcodeScanner.__cleanup_override = true;
 
     erpnext.utils.BarcodeScanner.prototype.clean_up = function () {
-        // Safe access to the active form object
         if (cur_frm && this.items_table_name) {
             cur_frm.refresh_field(this.items_table_name);
         }
